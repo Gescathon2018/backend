5ed258e285fb9683905c5f6332da6b9b3c211440
darinez <darinez@gaa9.com>
2018-07-07 13:28:31 +0200

    Dockerize

--- compose/django/start-server.sh
+++ compose/django/start-server.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 python manage.py migrate
-python manage.py collectstatic --noinput
 
-uwsgi --socket :8001 --wsgi-file /app/ara/wsgi.py --py-autoreload 3 -b 16384
+uwsgi --socket :8001 --wsgi-file /app/backend/wsgi.py --py-autoreload 3 -b 16384
